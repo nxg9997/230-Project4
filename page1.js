59f2ccdbb754b0c7ecae5b05a406b2dc4e485813
nxg9997 <nxg9997@rit.edu>
2017-12-04 13:13:32 -0500

    added Victor library

--- page1.js
+++ page1.js
@@ -1,6 +1,15 @@
 let tri = "tri.svg";
 let triNum = 10;
+let triHeight = 72;
+let triWidth = 44;
+
 let triArr;
+let triPosArr;
+let triVelArr;
+let triAccArr;
+
+let maxSpeed = 10;
+let maxForce = 2;
 
 InstantiateTris();
 
@@ -16,5 +25,31 @@ function InstantiateTris(){
     }
     
     triArr = document.querySelectorAll("img");
+
+    for(let i = 0; i < triNum; i++){
+        triArr[i].style.position = 'absolute';
+        let newTop = 0 - triHeight / 2;
+        let newLeft = 0 - triWidth / 2;
+        triArr[i].style.top = "" + newTop + "px";
+        triArr[i].style.left = "" + newLeft + "px";
+    }
+
+    triAccArr = new Array(triArr.length);
+    triPosArr = new Array(triArr.length);
+    triVelArr = new Array(triArr.length);
+
+    for (let i = 0; i < triNum; i++){
+        triAccArr = new Victor(0, 0);
+        triVelArr = new Victor(0, 0);
+        triPosArr = new Victor(0, 0);
+    }
+
+}
+
+function FollowMouse(){
+
+}
+
+function Seek(){
 
 }
